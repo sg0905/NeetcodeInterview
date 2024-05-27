1cebbe3bc242fbc5c88ba98bf80c570f5a234b98
Shubham Kumar Gupta <shubhamthe9@gmail.com>
2024-05-28 00:57:52 +0530

    Added adding integer number including negative numbers too

--- SumInteger.java
+++ SumInteger.java
@@ -1,16 +1,34 @@
+//Given two integers a and b, return the sum of the two integers without using the + and - operators.
+
 public class SumInteger{
     void run(int a, int b){
         // let a = 4 , b = 5
-        while(b != 0) {
-            a++;
-            b--;
+        
+        if( b > 0){
+            while(b != 0) {
+                a++;
+                b--;
+            }  
         }
-        System.out.print("Sum of two number without + operator : " + a);
+        else if ( b < 0) {
+            while ( b != 0) {
+                b++;
+                a--;
+            }
+        }
+        
+        System.out.println("Sum of two number without + operator : " + a);
     }
         public static void main(String args[]) {
-            int a = 4, b =5;
             SumInteger si = new SumInteger();
+            int a = 4, b =5;
             si.run(a, b);
+
+            
+            si.run(0, -1);
+
+            
+            si.run(-5, -4);
         }
         
     }
